21d11cfc260633305e4b467fc9582d9cd8fbc8a1
Atanas Chuchulev <atanas.v4@gmail.com>
2020-03-05 17:01:20 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,16 @@
 # The default provider configuration
 provider "aws" {
   region = "us-east-1"
+  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 # Additional provider configuration for west coast region
 provider "aws" {
   alias  = "west"
   region = "us-west-2"
+  
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
